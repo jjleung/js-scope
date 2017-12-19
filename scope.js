cfe42d3d4ba20a217e5123b1485fe8ba85f695d9
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2017-12-18 18:21:05 -1000

    done

--- scope.js
+++ scope.js
@@ -2,11 +2,11 @@
 var isCookieJarOpen = null;
 
 function closeLid() {
-  /* answer here */
+  isCookieJarOpen = false;
 }
 
 function openLid() {
-  /* answer here */
+  isCookieJarOpen = true;
 }
 
 
@@ -17,7 +17,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + " " + world;
   }
   return innerFunction();
 }
@@ -31,8 +31,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix[i].length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
@@ -49,7 +49,7 @@ function sendDataToClient() {
   }
 
   function authenticateUser(obj, username) {
-    var userObject = {
+    var userObject2 = {
       handle: 'morpheus',
       authenticated: false
     };
@@ -58,7 +58,7 @@ function sendDataToClient() {
       userObject.authenticated = true;
       return userObject
     } else {
-      return userObject
+      return userObject2
     }
   }
   authenticateUser(userObject, 'neo')
